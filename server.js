6e8d28b72bcef534b2bcd39d4152191332b47b45
alexander-hp <hdezppalex@gmail.com>
2022-07-28 12:55:01 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+var app = express();
+
+app.use(router);
+
+app.use('/', function (req, res) {
+  res.send('hola');
+});
+
+router.get('/message', (req, res) => {
+  res.send('Respuesta desde tipo get');
+});
+
+router.post('/message', (req, res) => {
+  res.send('Respuesta desde tipo post');
+});
+
+app.listen(3000);
+console.log('aplicacion correidno en el puerto 3000');
